4fa6c9ad5a8ced856d0e887403380e2c77184b96
Jan Xie <jan.h.xie@gmail.com>
2014-03-22 15:11:54 +0800

    formalize sum representation before hash

--- lib/liability-proof/tree/node.rb
+++ lib/liability-proof/tree/node.rb
@@ -8,7 +8,18 @@ module LiabilityProof
       end
 
       def sum_string
-        sum.to_s('F')
+        decimal_to_digits sum
+      end
+
+      def decimal_to_digits(d)
+        _, significant_digits, _, exponent = d.split
+        if d.zero?
+          '0'
+        elsif exponent >= significant_digits.size
+          d.to_i.to_s
+        else
+          d.to_s('F')
+        end
       end
 
     end
